acac72d681eef19feca7ee5a377fea7a0b7442ba
Jason Swett <jason@benfranklinlabs.com>
2017-02-25 11:27:23 -0500

    Add booksOrderedByTitle() and booksOrderedByYearPublished().

--- 01-typescript/index.ts
+++ 01-typescript/index.ts
@@ -1,22 +1,26 @@
 class Author {
-  books: Book[] = [];
+  public books: Book[] = [];
 
   constructor(private name: string) { }
 
   addBook(book: Book) {
     this.books.push(book);
   }
+
+  booksOrderedByTitle() { }
+  booksOrderedByYearPublished() { }
 }
 
 class Book {
-  constructor(public title: string) { }
+  constructor(public title: string, private yearPublished: number) { }
 }
 
 window.onload = function() {
   var author = new Author('John Steinbeck');
 
-  author.addBook(new Book('Of Mice and Men'));
-  author.addBook(new Book('The Grapes of Wrath'));
+  author.addBook(new Book('Of Mice and Men', 1937));
+  author.addBook(new Book('The Grapes of Wrath', 1939));
+  author.addBook(new Book('East of Eden', 1952));
 
   author.books.forEach(function(book) {
     console.log(book.title);
