8fb9c6aa2aa486cf5f03faeb2f7e6e4c56938927
Jason Swett <jason@benfranklinlabs.com>
2017-03-01 09:04:11 -0500

    Add semicolon.

--- 02-angular-hello-world/src/main.ts
+++ 02-angular-hello-world/src/main.ts
@@ -2,4 +2,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule);
