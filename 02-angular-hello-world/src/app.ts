cee40b1c891020d9087a0b4af539dc0287477a4a
Jason Swett <jason@benfranklinlabs.com>
2017-03-01 08:35:02 -0500

    Add lab 2.

---
+++ 02-angular-hello-world/src/app.ts
@@ -0,0 +1,21 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Hello!</h1>
+  `
+})
+export class App implements OnInit {
+  constructor() {}
+  
+  ngOnInit() {}
+}
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ App ],
+  bootstrap: [ App ]
+})
+export class AppModule {}
