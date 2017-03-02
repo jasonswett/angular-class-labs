090f597fcac6aba4ff2b8149f77b500bb622f522
Jason Swett <jason@benfranklinlabs.com>
2017-03-02 15:27:23 -0500

    Add lab 7.

---
+++ 07-input/home-library/src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  book: { id: number, name: string };
+
+  constructor(private route: ActivatedRoute, private bookService: BookService) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+        this.book = this.bookService.getBook(+params['id'])
+      });
+  }
+
+}
