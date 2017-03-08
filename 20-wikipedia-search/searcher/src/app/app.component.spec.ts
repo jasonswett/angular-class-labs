5df8c071b4dc7b8acfe91fea8e06f7b40616bdeb
Jason Swett <jason@benfranklinlabs.com>
2017-03-08 08:21:27 +0200

    Make test pass.

--- 20-wikipedia-search/searcher/src/app/app.component.spec.ts
+++ 20-wikipedia-search/searcher/src/app/app.component.spec.ts
@@ -2,6 +2,10 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { WikipediaService } from './wikipedia.service';
+
+class MockWikipediaService {
+}
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,7 +13,14 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+    }).overrideComponent(AppComponent, {
+      set: {
+        providers: [
+          { provide: WikipediaService, useClass: MockWikipediaService }
+        ]
+      }
     });
+
     TestBed.compileComponents();
   });
 
@@ -17,18 +28,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });
