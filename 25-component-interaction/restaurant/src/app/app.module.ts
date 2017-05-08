bba6f5c37132af94e7228404c5934c5d6d443a0c
Jason Swett <jason@benfranklinlabs.com>
2017-05-08 10:18:36 -0400

    Add OrderListComponent.

--- 25-component-interaction/restaurant/src/app/app.module.ts
+++ 25-component-interaction/restaurant/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
